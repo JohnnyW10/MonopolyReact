373e874bb84ff05cfb31a3a221fea930a81f2aba
JohnnyW10 <jw.wolan@gmail.com>
2022-06-03 00:22:47 +0200

    CheckCard function update

--- src/Javascript/form.js
+++ src/Javascript/form.js
@@ -15,6 +15,8 @@ export default function Form() {
 
 function CheckCard(card) {
   let normalCard = ['Name', 'Cost', 'With one house', 'With two', 'With three', 'With four', 'With hotel']
+  let trainCard = ['Name', 'Cost']
+  let specialCard = ['Name', 'Rule', 'Pay']
 
   if(card.value === '') {
     return ''
@@ -22,7 +24,25 @@ function CheckCard(card) {
     return(
       <div class="valueCard">
         <form>
-          {normalCard.map(el => <p><label>{el}: </label><input type="text"></input></p>)}
+          {normalCard.map(el => <p key={el.indexOf}><label>{el}: </label><input type="text"></input></p>)}
+          <button id="addCard">Add Card</button>
+        </form>
+      </div>
+    )
+  } else if (card.value === 'Koleje') {
+    return(
+      <div class="valueCard">
+        <form>
+          {trainCard.map(el => <p key={el.indexOf}><label>{el}: </label><input type="text"></input></p>)}
+          <button id="addCard">Add Card</button>
+        </form>
+      </div>
+    )
+  } else if (card.value === 'Specjalna') {
+    return(
+      <div class="valueCard">
+        <form>
+          {specialCard.map(el => <p key={el.indexOf}><label>{el}: </label><input type="text"></input></p>)}
           <button id="addCard">Add Card</button>
         </form>
       </div>
