f788581846713c7cf31514369de6278a98386648
JohnnyW10 <jw.wolan@gmail.com>
2022-06-03 16:18:24 +0200

    new component

---
+++ src/Javascript/createCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function createCard(card, cardInformation) {
+  console.log(card)
+  console.log(cardInformation)
+  console.log('siema')
+  return 0
+}
--- src/Javascript/form.js
+++ src/Javascript/form.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useState } from 'react';
+import createCard from "./createCard";
 
 export default function Form() {
   const [card, setCard] = useState('')
-  const Btns = ['Normalna Karta', 'Koleje', 'Specjalna']
+  const Btns = ['Normal', 'TrainStation', 'Specjal']
 
   return(
     <div id="btnsBox">
@@ -14,36 +15,37 @@ export default function Form() {
 }
 
 function CheckCard(card) {
-  let normalCard = ['Name', 'Cost', 'With one house', 'With two', 'With three', 'With four', 'With hotel']
+  let normalCard = ['Name', 'Cost', 'one-house', 'two-house', 'three-house', 'four-house', 'Hotel']
   let trainCard = ['Name', 'Cost']
   let specialCard = ['Name', 'Rule', 'Pay']
+  let cardd = card.value
 
   if(card.value === '') {
     return ''
-  } else if (card.value === 'Normalna Karta') {
+  } else if (card.value === 'Normal') {
     return(
       <div class="valueCard">
         <form>
-          {normalCard.map(el => <p key={el.indexOf}><label>{el}: </label><input type="text"></input></p>)}
-          <button id="addCard">Add Card</button>
+          {normalCard.map(el => <p key={el.indexOf}><label>{el}: </label><input id={el} type="text"></input></p>)}
+          <button class="addCard" onClick={createCard(cardd, normalCard)}>Add Card</button>
         </form>
       </div>
     )
-  } else if (card.value === 'Koleje') {
+  } else if (card.value === 'TrainStation') {
     return(
       <div class="valueCard">
         <form>
           {trainCard.map(el => <p key={el.indexOf}><label>{el}: </label><input type="text"></input></p>)}
-          <button id="addCard">Add Card</button>
+          <button class="addCard">Add Card</button>
         </form>
       </div>
     )
-  } else if (card.value === 'Specjalna') {
+  } else if (card.value === 'Specjal') {
     return(
       <div class="valueCard">
         <form>
           {specialCard.map(el => <p key={el.indexOf}><label>{el}: </label><input type="text"></input></p>)}
-          <button id="addCard">Add Card</button>
+          <button class="addCard">Add Card</button>
         </form>
       </div>
     )
